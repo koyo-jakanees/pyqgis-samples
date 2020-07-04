14b93f609432bfb4466f4d494195e9c27c1435a0
koyo-jakanees <davince.koyo@gmail.com>
2020-07-04 18:56:45 +0300

    Change rendererV2==> renderer| QgstyleV2==> QgsStyle in gui/qgis-sample-Qgs25DRendererWidget.py

--- gui/qgis-sample-Qgs25DRendererWidget.py
+++ gui/qgis-sample-Qgs25DRendererWidget.py
@@ -1,21 +1,21 @@
 # coding: utf-8
-from qgis.core import QgsStyleV2
+from qgis.core import QgsStyle
 from qgis.gui import Qgs25DRendererWidget
 from qgis.utils import iface
 
 layer = iface.activeLayer()
 renderer_25D_widget = Qgs25DRendererWidget(
     layer,
-    QgsStyleV2.defaultStyle(),
-    layer.rendererV2()
+    QgsStyle.defaultStyle(),
+    layer.renderer()
 )
 
 renderer_25D_widget.show()
 
 renderer_25D_widget_create = Qgs25DRendererWidget.create(
     layer,
-    QgsStyleV2.defaultStyle(),
-    layer.rendererV2()
+    QgsStyle.defaultStyle(),
+    layer.renderer()
 )
 
 renderer_25D_widget_create.show()
