0f28ada41af9bfa4b0bac025a04b54bf23cd62e9
koyo-jakanees <davince.koyo@gmail.com>
2020-07-04 18:06:27 +0300

    change QgsAttributeForm.Mode to QgsAttributeEditorContext.Mode

--- gui/qgis-sample-QgsAttributeForm.py
+++ gui/qgis-sample-QgsAttributeForm.py
@@ -4,6 +4,6 @@ from qgis.utils import iface
 
 layer = iface.activeLayer()
 attribute_form = QgsAttributeForm(layer, next(layer.getFeatures()))
-attribute_form.setMode(QgsAttributeForm.SingleEditMode)  # SingleEditMode, AddFeatureMode, MultiEditMode, SearchMode
+attribute_form.setMode(mode=0)  # SingleEditMode:0, AddFeatureMode:1, MultiEditMode:2, SearchMode:3
 
 attribute_form.show()
