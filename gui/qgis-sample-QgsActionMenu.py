92950e5197d291655321f989fab6c3bb929cc019
koyo-jakanees <davince.koyo@gmail.com>
2020-07-08 11:11:42 +0300

    refactor  with comments

--- gui/qgis-sample-QgsActionMenu.py
+++ gui/qgis-sample-QgsActionMenu.py
@@ -1,15 +1,22 @@
 # coding: utf-8
 from __future__ import print_function
+# from qgis.PyQt .QtWidgets import QWidget
 from qgis.gui import QgsActionMenu
 from qgis.utils import iface
 
+# qwt = QWidget()
 layer = iface.activeLayer()
-if iface.activeLayer().actions().size() > 0:
+if iface.activeLayer().actions():
+#  AttributeError: 'QgsActionManager' object has no attribute 'size' (Manual refactor!!!)
+# actionScope: str = 'Canvas, Field, Feature, Layer' or '' to list all actionScopes
+# QgsActionMenu(layer: QgsVectorLayer, feature: QgsFeature, actionScope: str, parent: QWidget = None
+# QgsActionMenu(layer: QgsVectorLayer, fid: int, actionScope: str, parent: QWidget = None)
+# :param fid: The feature id of the feature for which this action will be run.
     features = layer.dataProvider().getFeatures()
     feat1 = next(features)
     action_menu = QgsActionMenu(
         layer,
-        feat1
+        feat1,'Feature'
     )
     print(feat1.id())
 
@@ -20,3 +27,4 @@ if iface.activeLayer().actions().size() > 0:
     print(feat2.id())
 else:
     print('No action for this layer')
+
