80128d267f6488b3b34444fcb36e7b34f2aabbbe
koyo-jakanees <davince.koyo@gmail.com>
2020-07-21 15:18:21 +0300

    refactor qgis-sample-QgsColorRampComboBox.py

--- gui/qgis-sample-QgsColorRampComboBox.py
+++ gui/qgis-sample-QgsColorRampComboBox.py
@@ -1,24 +1,25 @@
 # coding: utf-8
 from __future__ import print_function
 from qgis.PyQt.QtWidgets import QDialog
-from qgis.core import QgsStyleV2
-from qgis.gui import QgsColorRampComboBox
+from qgis.core import QgsStyle, QgsColorRamp
+from qgis.gui import QgsColorRampButton # replaces QgsColorRampComboBox
 
 # Create dialog and resize it
 new_dialog = QDialog()
 new_dialog.resize(800, 600)
 
-style = QgsStyleV2().defaultStyle()
+# style = QgsStyle().defaultStyle() API3 no longer uses the defualt QgsStyle
 
-color_ramp_combo_box = QgsColorRampComboBox(new_dialog)
-color_ramp_combo_box.populate(style)
+color_ramp_combo_box = QgsColorRampButton(new_dialog)
+color_ramp_combo_box.setToDefaultColorRamp() 
+# accepts '.setRandomColorRamp(), .setToDefaultColorRamp(), .setToNull()'
 
 
 def on_color_ramp_changed():
-    print(color_ramp_combo_box.currentColorRamp())
-    print(color_ramp_combo_box.currentText())
+    print(color_ramp_combo_box.colorRamp())
+    print(color_ramp_combo_box.colorRampName())
     print("Color ramp changed")
 
-color_ramp_combo_box.currentIndexChanged.connect(on_color_ramp_changed)
+color_ramp_combo_box.colorRampChanged.connect(on_color_ramp_changed)
 
 new_dialog.show()
