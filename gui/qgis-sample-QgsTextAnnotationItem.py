05a9fd614c3aecad8986e20e4b5f13364b1f3ab6
koyo-jakanees <davince.koyo@gmail.com>
2020-07-29 20:38:52 +0300

    api change from annotationItem to annotation

--- gui/qgis-sample-QgsTextAnnotationItem.py
+++ gui/qgis-sample-QgsTextAnnotationItem.py
@@ -1,26 +1,39 @@
-from builtins import str
 # coding: utf-8
 from qgis.PyQt.QtCore import QSizeF
-from qgis.PyQt.QtGui import QColor
-from qgis.core import QgsPoint
-from qgis.gui import QgsTextAnnotationItem
+from qgis.PyQt.QtGui import QColor, QTextDocument
+from qgis.core import QgsPointXY, QgsTextAnnotation
+
+# from qgis.gui import
 from qgis.utils import iface
 
 canvas = iface.mapCanvas()
-text_annotation_item = QgsTextAnnotationItem(canvas)
-X, Y = float(3), float(45)
-point = QgsPoint(X, Y)
-text_annotation_item.setMapPosition(point)
-text_annotation_item.setFrameSize(QSizeF(300, 200))
-text_annotation_item.setFrameColor(QColor(0, 255, 0))
-text_annotation_item.setFrameBackgroundColor(QColor(128, 128, 128))
+crs = QgsCoordinateReferenceSystem("EPSG:4326")
+text_annotation = QgsTextAnnotation(canvas)
+X, Y = float(37.0), float(0.1)
+point = QgsPointXY(X, Y)
+text_annotation.setMapLayer(iface.activeLayer())
+text_annotation.setMapPositionCrs(crs)
+text_annotation.setMapPosition(point)
+text_annotation.setFrameSize(QSizeF(300, 200))
+# text_annotation.setFrameColor(QColor(0, 255, 0))
+# text_annotation.setFrameBackgroundColor(QColor(128, 128, 128))
 text_document = QTextDocument()
 html_content = "<b>New annotation</b>"
 font_color, font_family, font_size = "#123456", "Times New Roman", 16
-text_document.setHtml('<font style="color:' + font_color +
-                      "; font-family:" + font_family + "; font-size: " +
-                      str(font_size) + 'px">' + html_content + "</font>")
-text_annotation_item.setDocument(text_document)
+text_document.setHtml(
+    '<font style="color:'
+    + font_color
+    + "; font-family:"
+    + font_family
+    + "; font-size: "
+    + str(font_size)
+    + 'px">'
+    + html_content
+    + "</font>"
+)
+text_annotation.setDocument(text_document)
+text_annotation.setVisible(True)
+print(text_annotation.mapPositionCrs())
 canvas.refresh()
 
 # Then remove
