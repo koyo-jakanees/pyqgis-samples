ebf60f17157b0eae258453319c12c0fb03bd2cc6
koyo-jakanees <davince.koyo@gmail.com>
2020-07-29 20:41:37 +0300

    add QgsProject import and ProjectScope variable

--- gui/qgis-sample-QgsVariableEditorWidget.py
+++ gui/qgis-sample-QgsVariableEditorWidget.py
@@ -1,17 +1,21 @@
 # coding: utf-8
 from __future__ import print_function
-from qgis.core import QgsExpressionContextUtils, QgsExpressionContext
+from qgis.core import QgsProject, QgsExpressionContextUtils, QgsExpressionContext
 from qgis.gui import QgsVariableEditorWidget
 from qgis.utils import iface
 
+
+project = QgsProject.instance()
 canvas = iface.mapCanvas()
 variable_editor_widget = QgsVariableEditorWidget()
 expression_context = QgsExpressionContext()
 expression_context.appendScope(QgsExpressionContextUtils.globalScope())
-expression_context.appendScope(QgsExpressionContextUtils.projectScope())
+expression_context.appendScope(QgsExpressionContextUtils.projectScope(project))
+# QgsExpressionContextUtils.projectScope(project: QgsProject)-> QgsExpressionContextScope
 expression_context.appendScope(QgsExpressionContextUtils.mapSettingsScope(
     canvas.mapSettings()
 ))
+# mapSettingsScope(mapSettings: QgsMapSettings) -> QgsExpressionContextScope
 variable_editor_widget.setContext(expression_context)
 
 variable_editor_widget.reloadContext()
