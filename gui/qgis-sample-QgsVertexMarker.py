2fd8c3b675f4fccc3e748ecd7832f667d9e23785
koyo-jakanees <davince.koyo@gmail.com>
2020-07-29 20:48:06 +0300

    change import to QgsPointXY

--- gui/qgis-sample-QgsVertexMarker.py
+++ gui/qgis-sample-QgsVertexMarker.py
@@ -1,16 +1,18 @@
 # coding: utf-8
 from qgis.PyQt.QtGui import QColor
 from qgis.utils import iface
-from qgis.core import QgsPoint
+from qgis.core import QgsPointXY
 from qgis.gui import QgsVertexMarker
 
 canvas = iface.mapCanvas()
 m = QgsVertexMarker(canvas)
-m.setCenter(QgsPoint(0, 0))
+m.setCenter(QgsPointXY(0, 0))
 
 m.setColor(QColor(0, 0, 255))
 m.setIconSize(7)
-m.setIconType(QgsVertexMarker.ICON_BOX)  # See the enum IconType from http://www.qgis.org/api/classQgsVertexMarker.html
+m.setIconType(QgsVertexMarker.ICON_BOX)  
+# See the enum IconType from http://www.qgis.org/api/classQgsVertexMarker.html: 
+# ICON_NONE, ICON_CROSS, ICON_CIRCLE, ICON_DOUBLE_TRIANGLE, ICON_RHOMBUS
 m.setPenWidth(3)
 
 m.hide()
